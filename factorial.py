9e136e23a4b0835c0624d7a260cd089aa00be3df
TheCalpolMan <43935058+TheCalpolMan@users.noreply.github.com>
2019-01-21 21:08:31 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -7,8 +7,9 @@ def factorial(reeeee):
     return(rhee)
 
 print(factorial(int(input("What do you want to be factorial'd?\n"))))
-
-for i in range(100):
+i = 0
+while 6 < 8:
+    i += 1
     os = 0
     it = 1
     while it <= len(str(factorial(i))):
@@ -17,11 +18,19 @@ for i in range(100):
         else:
             break
         it += 1
+    verycool = 1
+    meant = 0
+    while i // (5 ** verycool) != 0:
+        meant += i // (5 ** verycool)
+        verycool += 1
+    print(str(meant) + " wow")
     if os == 1:
         print(str(factorial(i)) + " is " + str(i) + "! and has 1 0")
     else:
         print(str(factorial(i)) + " is " + str(i) + "! and has " + str(os) + " 0's")
-    if os == (i // 5 + i // 25):
-        print("True " + str(i // 5 + i // 25))
+    if os == meant:
+        print("True " + str(meant))
     else:
-        print("False " + str(i // 5 + i // 25))
+        print("False " + str(meant))
+        break
+print("oh noes")
